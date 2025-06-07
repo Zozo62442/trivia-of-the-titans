476b42e30275e14200b035a4a078bb90b6d1ca1d
Zozo62442 <zoe.rotterr@gmail.com>
2025-06-07 11:02:48 +0200

    Add welcome message and intro inside start_quiz function

--- game.py
+++ game.py
@@ -3,6 +3,13 @@ from utils import get_user_choice, clear_screen
 import time
 
 def start_quiz(username):
+    """
+    Runs the main quiz game loop.
+    """
+    clear_screen()
+    print(f"🎉 Welcome to Trivia of Titans {username}! \n")
+    print("You'll be asked 10 mythology questions. Choose the correct answer by typing A, B, C, or D (it is case sensitive)!\n")
+    time.sleep(2)
     # 1. Fetch questions
     # 2. Loop through them
     # 3. Display each question and choices
